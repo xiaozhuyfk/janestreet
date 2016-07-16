24bb57bedc6adf66023ebdf8c5e9ba8ef0b91249
Kai Kang <kaik1@andrew.cmu.edu>
2016-07-16 12:01:27 -0700

    Update

--- book.py
+++ book.py
@@ -1,15 +1,42 @@
 class Book(object):
-    def __init__(self):
-        self.book = dict()
+	
+	def __init__(self):
+		self.book = dict()
+		self.BUY = 0
+		self.SELL = 1
+		
+	def getBuyList(self, symbol):
+		if symbol in self.book:
+			return self.book[symbol][self.BUY]
+		else:
+			return []
+	
+	def getSellList(self, symbol):
+		if symbol in self.book:
+			return self.book[symbol][self.SELL]
+		else:
+			return []
+	
+	def show(self):
+		print "--------------BOOK---------------"
+		for symbol in self.book:
+			print symbol
+			print self.book[symbol][self.BUY]
+			print self.book[symbol][self.SELL]
+			print "---------------------------"
+		print "--------------END----------------"
 
-    def getBuyList(self, symbol):
-        pass
-
-    def getSellList(self, symbol):
-        pass
-
-    def getHighestBuy(self, symbol):
-        pass
-
-    def getLowestSell(self, symbol):
-        pass
+	def getHighestBuyPrice(self, symbol):
+		if symbol not in self.book:
+			return -1
+		f = lambda x, y: if x[0] > y[0] then x else y
+		return reduce(f, self.book[symbol][self.BUY])[0]
+	
+	def getLowestSellPrice(self, symbol):
+		if symbol not in self.book:
+			return -1
+		f = lambda x, y: if x[0] < y[0] then x else y
+		return reduce(f, self.book[symbol][self.SELL])[0]
+	
+	def _update(self, symbol, buyList, sellList):
+		self.book[symbol] = (buyList, sellList)
